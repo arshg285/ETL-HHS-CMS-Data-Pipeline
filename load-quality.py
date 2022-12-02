776b27ffb2e5dbcaadcfff957f93557a59829684
arshg285 <arshg2811@gmail.com>
2022-12-02 00:47:03 -0500

    Modified the code to load CMS data files

--- load-quality.py
+++ load-quality.py
@@ -7,7 +7,13 @@ from credentials import username, password
 import sys
 from data_cleaning import data_cleaning_hginfo
 
-cms = data_cleaning_hhs('/Users/zoe/Desktop/Data engineering/Project/data_engineering_project/hhs_weekly_data_files/' + sys.argv[2])
+# Data loading and cleaning
+# date = sys.argv[1]
+# hhs_file = sys.argv[2]
+date = '2021-07-01'
+cms_file = 'Hospital_General_Information-2021-07.csv'
+file = str('/Users/arshmacbook/Desktop/36-614/Project/hospital_quality_files/' + cms_file)
+cms = data_cleaning_hginfo(date, file)[:5]
 
 # Establishing SQL connection
 conn = psycopg.connect(
@@ -26,33 +32,43 @@ cur = conn.cursor()
 conn = psycopg.connect(
     host = "sculptor.stat.cmu.edu",
     dbname = "arshg",
-    user = %(username)s,
-    password = %(password)s,
-    params = {'username' : username,
-              'password' : password}
+    user = username,
+    password = password
 )
 
 # Creating a cursor object
 cur = conn.cursor()
-
-# Getting all hospital IDs to compare columns with
-cur.execute("SELECT hospital_pk FROM address")
-existing_hospitals = pd.Series(cur.fetchall())
 
 error_rows_cms = pd.DataFrame()
 num_rows_successfully_inserted_cms = 0
 num_rows_error_cms = 0
 
-## Using try-except blocks to perform transactions and insert rows into the database
+# Using try-except blocks to perform transactions and insert rows into the database
 with conn.transaction():
     for column_name, row in cms_hospital_quality.iterrows():
-        
-        # If the current hospital is not already in the table, we add it
-        if `row.Facility ID` not in existing_hospitals:
-        
-            # Make a new savepoint: Adding values to address table
-            try:
-                with conn.transaction():
+        try:
+            with conn.transaction():
+                    
+                # If a hospital already exists in the table then update existing values
+                cur.execute("SELECT hospital_pk FROM address")
+                existing_hospitals = pd.Series(cur.fetchall())
+                if row.hospital_pk in existing_hospitals:
+                    
+                    # Update info in ratings table
+                    cur.execute("UPDATE ratings "
+                                "SET overall_quality_rating = %(overall_quality_rating)s, "
+                                "type = %(type)s, "
+                                "emergency_services_provided = %(emergency_services_provided)s "
+                                "WHERE hospital_pk = %(hospital_pk)s",
+                                {'overall_quality_rating' : float(row.overall_quality_rating),
+                                 'type' : str(row.type),
+                                 'emergency_services_provided' : bool(row.emergency_services_provided),
+                                 'hospital_pk' : str(row.hospital_pk)})
+                    
+                # If the hospital does not exist in the table then insert it
+                else:
+                    
+                    # Add row to address table
                     cur.execute("INSERT into address "
                                 "(hospital_name, "
                                 "hospital_pk, "
@@ -66,59 +82,29 @@ with conn.transaction():
                                 "%(city)s, "
                                 "%(state)s, "
                                 "%(zip)s)",
-                                params = {'hospital_name' : `row.Facility Name`,
-                                          'hospital_pk' : `row.Facility ID`,
-                                          'address' : row.Address,
-                                          'city' : row.City,
-                                          'state' : row.State,
-                                          'zip' : `row.ZIP Code`})
+                                {'hospital_name' : str(row.hospital_name),
+                                 'hospital_pk' : str(row.hospital_pk),
+                                 'address': str(row.address),
+                                 'city': str(row.city),
+                                 'state': str(row.state),
+                                 'zip': str(row.zip)})
 
-            except Exception as e:
-                error_rows_cms.append(e)
-                num_rows_error_cms += 1
-                print("Insert failed")
-
-            else:
-                num_rows_successfully_inserted_cms += 1
-
-            # Make a new savepoint: Adding values to capacity_info table
-            try:
-                with conn.transaction():
+                    # Add rows to capacity info table
                     cur.execute("INSERT into capacity_info "
                                 "(hospital_pk) "
                                 "VALUES (%(hospital_pk)s)",
-                                params = {'hospital_pk' : `row.Facility ID`})
+                                {'hospital_pk' : str(row.hospital_pk)})
 
-            except Exception as e:
-                error_rows_cms.append(e)
-                num_rows_error_cms += 1
-                print("Insert failed")
-
-            else:
-                num_rows_successfully_inserted_cms += 1
-
-            # Make a new savepoint: Adding values to covid_info table
-            try:
-                with conn.transaction():
+                    # Add rows to covid info table
                     cur.execute("INSERT into covid_info "
                                 "(hospital_pk, "
                                 "hospital_name) "
                                 "VALUES (%(hospital_pk)s), "
                                 "%(hospital_name)s)",
-                                params = {'hospital_pk' : `row.Facility Name`,
-                                          'hospital_name' : `row.Facility ID`})
+                                {'hospital_pk' : str(row.hospital_pk),
+                                 'hospital_name' : str(row.hospital_name)})
 
-            except Exception as e:
-                error_rows_cms.append(e)
-                num_rows_error_cms += 1
-                print("Insert failed")
-
-            else:
-                num_rows_successfully_inserted_cms += 1
-
-            # Make a new savepoint: Adding values to ratings table
-            try:
-                with conn.transaction():
+                    # Add rows to ratings table
                     cur.execute("INSERT into ratings "
                                 "(hospital_name, "
                                 "hospital_pk, "
@@ -130,43 +116,19 @@ with conn.transaction():
                                 "%(emergency_services_provided)s, "
                                 "%(type)s, "
                                 "%(emergency_services_provided)s)",
-                                params = {'hospital_name' : `row.Facility Name`,
-                                          'hospital_pk' : `row.Facility ID`,
-                                          'overall_quality_rating' : `row.Hospital overall rating`,
-                                          'type' : `row.Hospital Ownership`,
-                                          'emergency_services_provided' : `row.Emergency Services`})
+                                {'hospital_name' : str(row.hospital_name),
+                                 'hospital_pk' : str(row.hospital_pk),
+                                 'overall_quality_rating' : float(row.overall_quality_rating),
+                                 'type' : str(row.type),
+                                 'emergency_services_provided' : bool(row.emergency_services_provided)})
 
-            except Exception as e:
-                error_rows_cms.append(e)
-                num_rows_error_cms += 1
-                print("Insert failed")
+        except Exception as e:
+            row = dict(row)
+            error_rows_cms = error_rows_cms.append(row, ignore_index = True)
+            num_rows_error_cms += 1
 
-            else:
-                num_rows_successfully_inserted_cms += 1
-                
-        # If the current hospital is already in the table, we update the ratings table
-        if `row.Facility ID` in existing_hospitals:
-            
-            # Make a new savepoint: Adding values to ratings table
-            try:
-                with conn.transaction():
-                    cur.execute("UPDATE ratings "
-                                "SET overall_quality_rating = %(overall_quality_rating)s, "
-                                "type = %(type)s, "
-                                "emergency_services_provided = %(emergency_services_provided)s "
-                                "WHERE hospital_pk = %(hospital_pk)s",
-                               params = {'overall_quality_rating' : `row.Hospital overall rating`,
-                                         'type' : `row.Hospital Ownership`,
-                                         'emergency_services_provided' `row.Emergency Services`: ,
-                                         'hospital_pk' : `row.Facility ID`})
-
-            except Exception as e:
-                error_rows_cms.append(e)
-                num_rows_error_cms += 1
-                print("Insert failed")
-
-            else:
-                num_rows_successfully_inserted_cms += 1
+        else:
+            num_rows_successfully_inserted_cms += 1
 
 # Committing the transaction
 conn.commit()
