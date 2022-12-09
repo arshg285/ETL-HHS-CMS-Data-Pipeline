39891d3c751c21ec5af309e6917296da57d0376b
Arsh Gupta <91312116+arshg285@users.noreply.github.com>
2022-12-09 15:33:55 -0500

    Update load-hhs.py

--- load-hhs.py
+++ load-hhs.py
@@ -10,15 +10,17 @@ warnings.filterwarnings('ignore')
 
 # Data loading and cleaning
 hhs_file = sys.argv[1]
-path = str('/Users/arshmacbook/Desktop/36-614/data_engineering_project/hhs_weekly_data_files/')  # Change this to the user directory
+path = str('/Users/arshmacbook/Desktop/36-614/data_engineering_project/\
+    hhs_weekly_data_files/')  # Change this to the user directory
 file = str(path + hhs_file)
 hhs = data_cleaning_hhs(file)
 
+# Establishing a SQL connection
 conn = psycopg.connect(
-    host = "sculptor.stat.cmu.edu",
-    dbname = cd.,  # Insert your dbname
-    user = cd.,  # Insert your username
-    password = cd.  # Insert your password
+    host="sculptor.stat.cmu.edu",
+    dbname=cd.arsh_dbname,  # Insert your dbname
+    user=cd.arsh_username,  # Insert your username
+    password=cd.arsh_password  # Insert your password
 )
 
 # Creating a cursor object
@@ -30,60 +32,65 @@ num_rows_error_hhs = 0
 
 start_time = time.time()
 
-# Using try-except blocks to perform transactions and insert rows into the database
+# Using try-except blocks to perform transactions and insert rows into the
+# database
 with conn.transaction():
     for column, row in hhs.iterrows():
         try:
             with conn.transaction():
-                
-                # If a hospital already exists in the table then update values in the address table
+
+                # If a hospital already exists in the table then update values
+                # in the address table
                 cur.execute("SELECT hospital_pk FROM address")
-                existing_hospitals = np.array([elem[0] for elem in pd.Series(cur.fetchall())])
+                existing_hospitals = np.array(
+                    [elem[0] for elem in pd.Series(cur.fetchall())]
+                    )
                 if row.hospital_pk in existing_hospitals:
 
                     # Update info in address table
                     cur.execute("UPDATE address "
-                                 "SET hospital_name = %(hospital_name)s, "
-                                 "address = %(address)s, "
-                                 "city = %(city)s, "
-                                 "state = %(state)s, "
-                                 "zip = %(zip)s, "
-                                 "fips_code = %(fips_code)s"
-                                 "WHERE hospital_pk = %(hospital_pk)s",
-                                 {'hospital_name' : str(row.hospital_name),
-                                  'hospital_pk' : str(row.hospital_pk),
-                                  'address' : str(row.address),
-                                  'city' : str(row.city),
-                                  'state' : str(row.state),
-                                  'zip' : str(row.zip),
-                                  'fips_code' : float(row.fips_code)})
+                                "SET hospital_name = %(hospital_name)s, "
+                                "address = %(address)s, "
+                                "city = %(city)s, "
+                                "state = %(state)s, "
+                                "zip = %(zip)s, "
+                                "fips_code = %(fips_code)s"
+                                "WHERE hospital_pk = %(hospital_pk)s",
+                                {'hospital_name': str(row.hospital_name),
+                                 'hospital_pk': str(row.hospital_pk),
+                                 'address': str(row.address),
+                                 'city': str(row.city),
+                                 'state': str(row.state),
+                                 'zip': str(row.zip),
+                                 'fips_code': float(row.fips_code)})
 
-                # If the hospital does not exist in the address table then insert it
+                # If the hospital does not exist in the address table then
+                # insert it
                 else:
 
                     # Add row to address table
                     cur.execute("INSERT into address "
-                                 "(hospital_name, "
-                                 "hospital_pk, "
-                                 "address, "
-                                 "city, "
-                                 "state, "
-                                 "zip, "
-                                 "fips_code) "
-                                 "VALUES (%(hospital_name)s, "
-                                 "%(hospital_pk)s, "
-                                 "%(address)s, "
-                                 "%(city)s, "
-                                 "%(state)s, "
-                                 "%(zip)s, "
-                                 "%(fips_code)s)",
-                                 {'hospital_name' : str(row.hospital_name),
-                                  'hospital_pk' : str(row.hospital_pk),
-                                  'address' : str(row.address),
-                                  'city' : str(row.city),
-                                  'state' : str(row.state),
-                                  'zip' : str(row.zip),
-                                  'fips_code' : float(row.fips_code)})
+                                "(hospital_name, "
+                                "hospital_pk, "
+                                "address, "
+                                "city, "
+                                "state, "
+                                "zip, "
+                                "fips_code) "
+                                "VALUES (%(hospital_name)s, "
+                                "%(hospital_pk)s, "
+                                "%(address)s, "
+                                "%(city)s, "
+                                "%(state)s, "
+                                "%(zip)s, "
+                                "%(fips_code)s)",
+                                {'hospital_name': str(row.hospital_name),
+                                 'hospital_pk': str(row.hospital_pk),
+                                 'address': str(row.address),
+                                 'city': str(row.city),
+                                 'state': str(row.state),
+                                 'zip': str(row.zip),
+                                 'fips_code': float(row.fips_code)})
 
                 # Add rows to capacity info table
                 cur.execute("INSERT into capacity_info "
@@ -103,14 +110,26 @@ with conn.transaction():
                             "%(pediatric_inpatient_bed_occupied)s, "
                             "%(total_icu_beds)s, "
                             "%(icu_beds_used)s)",
-                            {'hospital_pk' : str(row.hospital_pk),
-                             'collection_week' : str(row.collection_week),
-                             'adult_hospital_beds' : float(row.all_adult_hospital_beds_7_day_avg),
-                             'pediatric_inpatient_beds' : float(row.all_pediatric_inpatient_beds_7_day_avg),
-                             'adult_hospital_inpatient_bed_occupied' : float(row.all_adult_hospital_inpatient_bed_occupied_7_day_coverage),
-                             'pediatric_inpatient_bed_occupied' : float(row.all_pediatric_inpatient_bed_occupied_7_day_avg),
-                             'total_icu_beds' : float(row.total_icu_beds_7_day_avg),
-                             'icu_beds_used' : float(row.icu_beds_used_7_day_avg)})
+                            {'hospital_pk': str(row.hospital_pk),
+                             'collection_week': str(row.collection_week),
+                             'adult_hospital_beds': float(
+                                row.all_adult_hospital_beds_7_day_avg
+                                ),
+                             'pediatric_inpatient_beds': float(
+                                row.all_pediatric_inpatient_beds_7_day_avg
+                                ),
+                             'adult_hospital_inpatient_bed_occupied': float(
+                                row.all_adult_hospital_inpatient_bed_occupied_7_day_coverage
+                                ),
+                             'pediatric_inpatient_bed_occupied': float(
+                                row.all_pediatric_inpatient_bed_occupied_7_day_avg
+                                ),
+                             'total_icu_beds': float(
+                                row.total_icu_beds_7_day_avg
+                                ),
+                             'icu_beds_used': float(
+                                row.icu_beds_used_7_day_avg
+                                )})
 
                 # Add rows to covid info table
                 cur.execute("INSERT into covid_info "
@@ -128,17 +147,25 @@ with conn.transaction():
                             "%(staffed_adult_icu_patients_confirmed_covid_7_day_avg)s, "
                             "%(total_icu_beds_7_day_avg)s, "
                             "%(icu_beds_used_7_day_avg)s)",
-                            {'hospital_pk' : str(row.hospital_pk),
-                             'hospital_name' : str(row.hospital_name),
-                             'collection_week' : str(row.collection_week),
-                             'inpatient_beds_used_covid_7_day_avg' : float(row.inpatient_beds_used_covid_7_day_avg),
-                             'staffed_adult_icu_patients_confirmed_covid_7_day_avg' : float(row.staffed_icu_adult_patients_confirmed_covid_7_day_avg),
-                             'total_icu_beds_7_day_avg' : float(row.total_icu_beds_7_day_avg),
-                             'icu_beds_used_7_day_avg' : float(row.icu_beds_used_7_day_avg)})
+                            {'hospital_pk': str(row.hospital_pk),
+                             'hospital_name': str(row.hospital_name),
+                             'collection_week': str(row.collection_week),
+                             'inpatient_beds_used_covid_7_day_avg': float(
+                                row.inpatient_beds_used_covid_7_day_avg
+                                ),
+                             'staffed_adult_icu_patients_confirmed_covid_7_day_avg': float(
+                                row.staffed_icu_adult_patients_confirmed_covid_7_day_avg
+                                ),
+                             'total_icu_beds_7_day_avg': float(
+                                row.total_icu_beds_7_day_avg
+                                ),
+                             'icu_beds_used_7_day_avg': float(
+                                row.icu_beds_used_7_day_avg
+                                )})
 
-        except Exception as e:
+        except Exception:
             row = dict(row)
-            error_rows_hhs = error_rows_hhs.append(row, ignore_index = True)
+            error_rows_hhs = error_rows_hhs.append(row, ignore_index=True)
             num_rows_error_hhs += 1
 
         else:
@@ -150,12 +177,16 @@ conn.commit()
 end_time = time.time()
 
 # Saving the data frame containing the error files to a separate CSV file
-error_rows_hhs.to_csv("Error rows in HHS data set.csv", index = False)
+error_rows_hhs.to_csv("Error rows in HHS data set.csv", index=False)
 
 # Printing the summary output
 print("\nTime taken:", round(((end_time - start_time) / 60), 2), "minutes\n")
-print("Number of rows successfully inserted:", round(num_rows_successfully_inserted_hhs / hhs.shape[0] * 100, 2), "%\n")
-print("Number of rows unable to be inserted due to errors:", round(num_rows_error_hhs / hhs.shape[0] * 100, 2), "%\n")
+print("Number of rows successfully inserted:", round(
+    num_rows_successfully_inserted_hhs / hhs.shape[0] * 100, 2
+    ), "%\n")
+print("Number of rows unable to be inserted due to errors:", round(
+    num_rows_error_hhs / hhs.shape[0] * 100, 2
+    ), "%\n")
 
 # Closing the SQL connection
 conn.close()
