6242186e835b9d2e957247d0889e3805b6de834b
KristinYang0320 <112430575+KristinYang0320@users.noreply.github.com>
2022-12-09 15:22:09 -0500

    update flake8

--- load-quality.py
+++ load-quality.py
@@ -2,26 +2,28 @@ import psycopg
 import pandas as pd
 import time
 import credentials as cd
-import numpy as np
 import sys
 from data_cleaning import data_cleaning_hginfo
 import warnings
+
 warnings.filterwarnings('ignore')
 
 # Data loading and cleaning
 input_date = sys.argv[1]
 cms_file = sys.argv[2]
-path = str('/Users/arshmacbook/Desktop/36-614/data_engineering_project/hospital_quality_files/')  # Change this to the user directory
+path = str(
+    '/Users/arshmacbook/Desktop/36-614/data_engineering_project/hospital_quality_files/')
+# Change this to the user directory
 file = str(path + cms_file)
 cms = data_cleaning_hginfo(input_date, file)
 cms['emergency_services_provided'] = cms['Emergency Services'].map({'Yes': True, 'No': False})
 
 # Establishing SQL connection
 conn = psycopg.connect(
-    host = "sculptor.stat.cmu.edu",
-    dbname = cd.,  # Insert your dbname
-    user = cd.,  # Insert your username
-    password = cd.  # Insert your password
+    host="sculptor.stat.cmu.edu",
+    dbname=cd.,  # Insert your dbname
+    user=cd.,  # Insert your username
+    password=cd.  # Insert your password
 )
 
 # Creating a cursor object
@@ -38,8 +40,9 @@ with conn.transaction():
     for column, row in cms.iterrows():
         try:
             with conn.transaction():
-                
-                # If a hospital already exists in the address table then add row to ratings table
+
+                # If a hospital already exists in the address table then
+                # add row to ratings table
                 cur.execute("INSERT into ratings "
                             "(hospital_name, "
                             "hospital_pk, "
@@ -53,16 +56,16 @@ with conn.transaction():
                             "%(overall_quality_rating)s, "
                             "%(type)s, "
                             "%(emergency_services_provided)s)",
-                            {'hospital_name' : str(row['Facility Name']),
-                             'hospital_pk' : str(row['Facility ID']),
-                             'collection_week' : str(row.collection_week),
-                             'overall_quality_rating' : float(row['Hospital overall rating']),
-                             'type' : str(row['Hospital Ownership']),
-                             'emergency_services_provided' : row.emergency_services_provided})
+                            {'hospital_name': str(row['Facility Name']),
+                             'hospital_pk': str(row['Facility ID']),
+                             'collection_week': str(row.collection_week),
+                             'overall_quality_rating': float(row['Hospital overall rating']),
+                             'type': str(row['Hospital Ownership']),
+                             'emergency_services_provided': row.emergency_services_provided})
 
         except Exception as e:
             row = dict(row)
-            error_rows_cms = error_rows_cms.append(row, ignore_index = True)
+            error_rows_cms = error_rows_cms.append(row, ignore_index=True)
             num_rows_error_cms += 1
 
         else:
@@ -74,12 +77,14 @@ conn.commit()
 end_time = time.time()
 
 # Saving the data frame containing the error files to a separate CSV file
-error_rows_cms.to_csv("Error rows in CMS data set.csv", index = False)
+error_rows_cms.to_csv("Error rows in CMS data set.csv", index=False)
 
 # Printing the summary output
 print("\nTime taken:", round(((end_time - start_time) / 60), 2), "minutes")
-print("Number of rows successfully inserted:", round(num_rows_successfully_inserted_cms / cms.shape[0] * 100, 2), "%\n")
-print("Number of rows unable to be inserted due to errors:", round(num_rows_error_cms / cms.shape[0] * 100, 2), "%\n")
+print("Number of rows successfully inserted:", round(num_rows_successfully_inserted_cms
+                                                     / cms.shape[0] * 100, 2), "%\n")
+print("Number of rows unable to be inserted due to errors:", 
+      round(num_rows_error_cms / cms.shape[0] * 100, 2), "%\n")
 
 # Closing the SQL connection
 conn.close()
